c827a7a6c9b4b9fe003d1141ef2f4be128ec7a5c
padsley <padsley@gmail.com>
2022-12-05 12:54:12 -0600

    uploading file from computer

---
+++ CAEN2ROOT.cpp
@@ -0,0 +1,186 @@
+#include <TFile.h>
+#include <TTree.h>
+#include <TString.h>
+#include <iostream>
+#include <sstream>
+
+void ProcessWaveforms(TString inFile, TString outFile);
+void ProcessSpectra(TString inFile, TString outFile);
+
+int EventWindow = 4;
+double TimeWindow = 100; //time window for making coincidence events (hopefully!)
+
+void CAEN2ROOT(TString inFile, TString outFile, bool waveforms=false)
+{
+    if(waveforms)
+        ProcessWaveforms(inFile, outFile);
+    else
+        ProcessSpectra(inFile, outFile);
+}
+
+void ProcessWaveforms(TString inFile, TString outFile)
+{
+    ifstream input;
+    input.open(inFile.Data());
+    
+    TFile *fout = new TFile(outFile.Data(),"RECREATE");
+    
+    TTree *trout = new TTree("waveforms","waveforms");
+    
+    int channel = -1;
+    
+    if(input.is_open())
+    {
+//         while()//check for the end of the files? how many samples?
+            
+            
+    }
+    
+    trout->Write();
+    fout->Close();
+    input.close();
+    
+    printf("Finished waveform conversion :)\n");
+}
+
+void ProcessSpectra(TString inFile, TString outFile)
+{
+    std::cout << "Processing listmode data" << std::endl;
+    
+    ifstream input;
+    input.open(inFile.Data());
+    
+    TFile *fout = new TFile(outFile.Data(),"RECREATE");
+    
+    TTree *trout = new TTree("listmode","listmode");
+    
+    std::vector<long> vChannel, vTimestamp, vADCValue;
+    
+    trout->Branch("channel",&vChannel);
+    trout->Branch("timestamp",&vTimestamp);
+    trout->Branch("adc",&vADCValue);
+    
+    if(input.is_open())
+    {
+        vChannel.clear();
+        vTimestamp.clear();
+        vADCValue.clear();
+        
+        std::vector<string> entry;
+        
+        std::string line, word;
+        
+        while(getline(input, line))
+        {
+            entry.clear();
+            vChannel.clear();
+            vTimestamp.clear();
+            vADCValue.clear();
+            
+            stringstream str(line);
+                                 
+            while(getline(str, word, ';'))
+            {
+                entry.push_back(word);
+                
+//                 std::cout << "word = " << word << std::endl;
+            }
+            
+//             std::cout << "entry.size() = " << entry.size() << std::endl;
+            
+            vChannel.push_back(stol(entry[0]));
+//             std::cout << "vChannel.size() = " << vChannel.size() << std::endl;
+            
+            vTimestamp.push_back(stol(entry[1]));
+//             std::cout << "vTimestamp.size() = " << vTimestamp.size() << std::endl;
+            
+            vADCValue.push_back(stol(entry[2]));
+//             std::cout << "vADCValue.size() = " << vADCValue.size() << std::endl;
+            
+            
+            trout->Fill();
+        }
+//                 std::cout << channel << "\t" << timestamp << "\t" << adc << std::endl;
+    }
+    else
+        std::cout << "Input file " << inFile.Data() << " doesn't seem to exist!" << std::endl;
+    
+    trout->Write();
+    
+    TTree *trent = new TTree("events","events");
+    int EventLength;
+    std::vector<long> EventTimes, EventADCs, EventChannels;
+    
+    trent->Branch("EventLength",&EventLength);
+    trent->Branch("EventTimes",&EventTimes);
+    trent->Branch("EventADCs",&EventADCs);
+    trent->Branch("EventChannels",&EventChannels);
+    
+    std::vector<long> *ReadChannel = 0, *ReadTime = 0, *ReadADC = 0;
+    TBranch *bReadChannel, *bReadTime, *bReadADC;
+    
+    TTree *ReadTrout = (TTree*)fout->Get("listmode");
+    
+    ReadTrout->SetBranchAddress("channel",&ReadChannel,&bReadChannel);
+    ReadTrout->SetBranchAddress("timestamp",&ReadTime,&bReadTime);
+    ReadTrout->SetBranchAddress("adc",&ReadADC,&bReadADC);
+    
+    long nentries = ReadTrout->GetEntries();
+    for(long i=0; i<nentries; i++)
+    {
+//         ReadTrout->Show(i);
+        ReadTrout->GetEntry(i);
+        
+        EventLength = 0;
+        EventTimes.clear();
+        EventADCs.clear();
+        EventChannels.clear();
+        
+//         int VectorSize = ReadChannel->size();
+//         std::cout << "VectorSize = " << VectorSize << std::endl;
+        
+//         std::cout << i << "\t" << ReadChannel->at(0) << std::endl;//just for testing
+        if(ReadChannel->size()>1)std::cout << "For event " << i << " there are multiple hits in one channel and I did not account for that!" << std::endl;
+        
+        long TimeFirstEvent = ReadTime->at(0);
+        
+        long TimeLastEvent = TimeFirstEvent;
+        
+        long LastEventNumber = i;
+        
+        EventLength++;
+        EventChannels.push_back(ReadChannel->at(0));
+        EventTimes.push_back(ReadTime->at(0));
+        EventADCs.push_back(ReadADC->at(0));
+        
+        for(long j=i+1; j<i+EventWindow; j++)//only look a few results ahead
+        {
+            ReadTrout->GetEntry(j);
+            
+//             std::cout << ReadTime->at(0) - TimeFirstEvent << std::endl;
+            
+            if(abs(ReadTime->at(0) - TimeFirstEvent)<TimeWindow)
+            {
+                EventLength++;
+                EventChannels.push_back(ReadChannel->at(0));
+                EventTimes.push_back(ReadTime->at(0));
+                EventADCs.push_back(ReadADC->at(0));
+                
+                LastEventNumber = j;
+            }
+        }
+        
+        i = LastEventNumber;
+        
+//         std::cout << "la la la" << std::endl;//this is supposed to be useful??
+        
+        trent->Fill();
+        
+    }
+    
+    trent->Write();
+    fout->Close();
+    
+    input.close();
+    
+}
