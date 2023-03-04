4a984d0b78f5265aeafe51950d3ec236a1487400
padsley <padsley@gmail.com>
2023-03-04 11:12:11 -0600

    PA added comments to the waveform bit

--- CAEN2ROOT.cpp
+++ CAEN2ROOT.cpp
@@ -1,15 +1,18 @@
+//header files to load useful stuff - TFile for output TTree, TString to handle filenames etc, iostream for input/output operations, sstream for something, maybe working with text again?
 #include <TFile.h>
 #include <TTree.h>
 #include <TString.h>
 #include <iostream>
 #include <sstream>
 
+//definitions of the two different ways of running the code, either with 'spectra' (meaning listmode = channel, time, energy) or waveforms (meaning the full sample wavefunction is stored)
 void ProcessWaveforms(TString inFile, TString outFile);
 void ProcessSpectra(TString inFile, TString outFile);
 
-int EventWindow = 4;
+int EventWindow = 4;//how far apart different events can be to be combined into one full event
 double TimeWindow = 100; //time window for making coincidence events (hopefully!)
 
+//to run the code, CAEN2ROOT(input file name, output file name, flag for if it's listmode or scope samples
 void CAEN2ROOT(TString inFile, TString outFile, bool waveforms=false)
 {
     if(waveforms)
@@ -20,84 +23,103 @@ void CAEN2ROOT(TString inFile, TString outFile, bool waveforms=false)
 
 void ProcessWaveforms(TString inFile, TString outFile)
 {
+  //open the input file
     ifstream input;
     input.open(inFile.Data());
-    
+
+    //make an output file - overwrite the old one
     TFile *fout = new TFile(outFile.Data(),"RECREATE");
-    
+
+    //make an output TTree with times, channels, samples as the branches
     TTree *trout = new TTree("waveforms","waveforms");
-    
+
+    //make the data structures to put into in and...
     std::vector<long> vTimestamp, vChannel, vSamples;
-    
+
+    //... link those data structures to the branches
     trout->Branch("channel",&vChannel);
     trout->Branch("timestamp",&vTimestamp);
     trout->Branch("samples",&vSamples);
-    
+
+    //declare a variable for the channel for each event - set to -1 because this CAN'T EVER HAPPEN so it's a good check if something fucked up
     int channel = -1;
     
     int counter = 0;//only here to I can try reading a few events at a time
     
-    TGraph *g = new TGraph();//for checking the shape
+    TGraph *g = new TGraph();//for checking/plotting the shape
     
     if(input.is_open())
     {
 //         while()//check for the end of the files? how many samples?
+      //make something to read the line into
         std::vector<string> entry;
-        
+        //make somethings to break the line up into
         std::string line, word;
         
-        while(getline(input, line) && counter<1)
+        while(getline(input, line) && counter<1)//this means only read one event because counter<1 - should fix this at some point... getline(input,line) reads a line from input into the variable 'line'
         {
-            std::cout << "New Entry" << std::endl;
-            
+	  std::cout << "New Entry" << std::endl;//printing stuff so I know things are happening
+
+	  //CLEAR THE VECTORS TO AVOID WRITING THE SAME EVENTS TO DISK LOADS OF TIME AND EXPLODING THE COMPUTER POW POW POW
             vTimestamp.clear();
             vChannel.clear();
             vSamples.clear();            
-            
+
+	    //read line into str because ?????
             stringstream str(line);
-            
+
+	    //break str up into words and store them
             while(getline(str, word, ';'))
             {
                 entry.push_back(word);
                 
                 std::cout << "word = " << word << std::endl;
             }
-            
+            //the timestamp is the first word (which is 0 because C-numbering)
             vTimestamp.push_back(stol(entry.at(0)));
+	    //the channel is the second word (at position 1)
             vChannel.push_back(stol(entry.at(1)));
 //             vSamples.push_back(entry.at(2));
-                
+
+	    //work out if the signal is positive or negative going (somehow??)
             bool NegativeSignal = false;
+	    //stol is string to long so it's converting a text string to a number (I believe)
             if(stol(entry.at(4))<0)NegativeSignal = true;
-            
+
+	    //I'm not entirely clear why I did this
             if(NegativeSignal)vSamples.push_back(-1*stol(entry.at(4)));
             else vSamples.push_back(stol(entry.at(4)));
-            
+
+	    //loop over the number of samples which is an integer at position 3 in the output line and save the waveform values to the nice vector setup for this porpoise
             for(int i=1;i<stoi(entry.at(2));i++)
             {
                 vSamples.push_back(stol(entry.at(i+4)));
             }
-            
+
+	    //save the waveform information into a TGraph that can be plotted if needed
             for(unsigned int i=0;i<vSamples.size()-2;i++)
             {
                 std::cout << vSamples.at(i) << std::endl;
-                g->SetPoint(g->GetN(),i,vSamples.at(i));
+                g->SetPoint(i,i,vSamples.at(i));
             }
-            
+
+	    //iterate the counter for checking events
             counter++;
+	    //fill the output TTree just saving each waveform separately
             trout->Fill();
         }
     }//everything up to this point is just reading in the waveforms but now we need to look at making proper energy values from them etc *le sigh*
     
     
-    
+    //draw the TGraph
     g->Draw("AP");
-    
+
+    //write the output TTree
     trout->Write();
-    fout->Close();
-    input.close();
+    fout->Close();//close the output
+    input.close();//close the input
     
-    printf("Finished waveform conversion :)\n");
+    printf("Finished waveform conversion :)\n");//wooooo done
 }
 
 void ProcessSpectra(TString inFile, TString outFile)
